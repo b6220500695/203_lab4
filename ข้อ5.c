6fc2daf2830b901b8918f3c232fe5b73898c2f82
mmint.m <pheeraya.p@ku.th>
2021-03-04 16:38:42 +0700

    5

--- ข้อ5.c
+++ ข้อ5.c
@@ -3,5 +3,17 @@
 int main(void){
    int m=0,x=0,g[100],a,b,c,d;
    char i[50];
-
-}
+   scanf("%d\n",&m);
+   if((m>0)&&(m<100)){
+   for(a=0;a<m;a++){
+   gets(i);
+   int j[3]={1,2,3};
+for(b=0;x[b]!='\0';b++){
+  if(i[b]=='A'){
+     x=j[1];
+      j[1]=j[0];
+      j[0]=x;
+  }
+  if(i[b]=='B')
+   x =j[2];
+  j[2]=j[1];
