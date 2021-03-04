ce9beb2f7f92549d4ae48f69d084ca742ddd7612
mmint.m <pheeraya.p@ku.th>
2021-03-04 15:31:10 +0700

    1

---
+++ 1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+   const int N = 55;
+   char str[10][55];
+   int len[10];
+   scanf("%s",str[0]);
+   len[0] = strlen(str[0]);
+      for(int i=1;i<4;i++)
+      {
+      scanf("%s",str[i]);
+      len[i] = strlen(str[i]);
+      }
+   }
+
