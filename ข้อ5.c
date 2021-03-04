8bc240274cb502c4680a2ffd9bdac21669b031f5
mmint.m <pheeraya.p@ku.th>
2021-03-04 17:00:52 +0700

    5รันใหม่

--- ข้อ5.c
+++ ข้อ5.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <conio.h>
-int main(void){
+int main(){
    int m=0,x=0,g[100],a,b,c,d;
    char i[50];
    scanf("%d\n",&m);
    if((m>0)&&(m<100)){
    for(a=0;a<m;a++){
-   gets(i);
+   fgets(i,50,stdin);
    int j[3]={1,2,3};
 for(b=0;i[b]!='\0';b++){
   if(i[b]=='A'){
